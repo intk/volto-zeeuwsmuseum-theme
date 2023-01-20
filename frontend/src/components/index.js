b31bd69079489684f8481ecedd89bd66b46136ba
Cihan Andac <cihanandac@gmail.com>
2023-01-20 16:59:51 +0300

    fixed problems about the block components

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,5 +1,6 @@
 /**
  * Add your components here.
+ *
  * @module components
  * @example
  * import Footer from './Footer/Footer';
@@ -8,3 +9,29 @@
  *   Footer,
  * };
  */
+
+import EmptylineViewBlock from './Blocks/Emptyline/View';
+import EmptylineEditBlock from './Blocks/Emptyline/Edit';
+import SocialBottomViewBlock from './Blocks/SocialBottom/View';
+import SocialBottomEditBlock from './Blocks/SocialBottom/Edit';
+import NutezienViewBlock from './Blocks/Nutezien/View';
+import NutezienEditBlock from './Blocks/Nutezien/Edit';
+import PhotoDescriptionViewBlock from './Blocks/PhotoDescription/View';
+import PhotoDescriptionEditBlock from './Blocks/PhotoDescription/Edit';
+import StoryBlockViewBlock from './Blocks/StoryBlock/View';
+import StoryBlockEditBlock from './Blocks/StoryBlock/Edit';
+import SliderPage from './View/SliderPage';
+
+export {
+  EmptylineViewBlock,
+  EmptylineEditBlock,
+  SocialBottomViewBlock,
+  SocialBottomEditBlock,
+  NutezienViewBlock,
+  NutezienEditBlock,
+  PhotoDescriptionViewBlock,
+  PhotoDescriptionEditBlock,
+  StoryBlockViewBlock,
+  StoryBlockEditBlock,
+  SliderPage,
+};
