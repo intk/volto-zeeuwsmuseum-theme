a1c899fc822bb2d02d6f51c6d2733c135c867249
Cihan Andac <cihanandac@gmail.com>
2023-03-21 23:24:55 +0300

    Fix: the bug for the props.content not found

--- frontend/src/customizations/components/theme/Breadcrumbs/Breadcrumbs.jsx
+++ frontend/src/customizations/components/theme/Breadcrumbs/Breadcrumbs.jsx
@@ -99,15 +99,17 @@ export class BreadcrumbsComponent extends Component {
   render() {
     let parentURL = '';
     let selfURL = '';
-    const parentID = this.props.parentData['@id'];
-    const selfID = this.props.content['@id'];
-    if (parentID !== undefined) {
-      const parts = parentID.split('/');
-      parentURL = '/' + parts.slice(3).join('/');
-    }
-    if (selfID !== undefined) {
-      const parts = selfID.split('/');
-      selfURL = '/' + parts.slice(3).join('/');
+    if (this.props.content != undefined) {
+      const parentID = this.props.parentData['@id'];
+      const selfID = this.props.content['@id'];
+      if (parentID !== undefined) {
+        const parts = parentID.split('/');
+        parentURL = '/' + parts.slice(3).join('/');
+      }
+      if (selfID !== undefined) {
+        const parts = selfID.split('/');
+        selfURL = '/' + parts.slice(3).join('/');
+      }
     }
 
     return (
