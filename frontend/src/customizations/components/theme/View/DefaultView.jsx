588733f8a2dfd490eeebcb83bfe4e038764898b8
Cihan Andac <cihanandac@gmail.com>
2023-05-09 08:33:33 +0300

    change the redirection at lrf

--- frontend/src/customizations/components/theme/View/DefaultView.jsx
+++ frontend/src/customizations/components/theme/View/DefaultView.jsx
@@ -43,17 +43,17 @@ const DefaultView = ({ content, intl, location }) => {
 
   //This section is for the redirection of the LRF page
   //uncomment for the activation of the redirection
-  // useEffect(() => {
-  //   const redirection = (event) => {
-  //     const languageToken = content.language?.token
-  //       ? content.language.token
-  //       : '';
-  //     if (content['@type'] === 'LRF') {
-  //       window.location.href = `https://www.zeeuwsmuseum.nl/${languageToken}`;
-  //     }
-  //   };
-  //   redirection();
-  // }, [content]);
+  useEffect(() => {
+    const redirection = (event) => {
+      const languageToken = content.language?.token
+        ? content.language.token
+        : '';
+      if (content['@type'] === 'LRF') {
+        window.location.href = `https://www.zeeuwsmuseum.nl/${languageToken}`;
+      }
+    };
+    redirection();
+  }, [content]);
 
   return hasBlocksData(content) ? (
     <div id="page-document" className="ui container">
