99d8a6831e61ff2ff9be491bae724a5a7d934af9
Cihan Andac <cihanandac@gmail.com>
2023-06-05 08:51:49 +0200

    Add nutezien view for the listing block

--- frontend/src/customizations/components/manage/Blocks/Listing/ListingBody.jsx
+++ frontend/src/customizations/components/manage/Blocks/Listing/ListingBody.jsx
@@ -11,19 +11,7 @@ import paginationRightSVG from '@plone/volto/icons/right-key.svg';
 import { useSelector } from 'react-redux';
 
 const ListingBody = withQuerystringResults((props) => {
-  const {
-    data = {},
-    isEditMode,
-    listingItems,
-    totalPages,
-    onPaginationChange,
-    variation,
-    currentPage,
-    prevBatch,
-    nextBatch,
-    isFolderContentsListing,
-    hasLoaded,
-  } = props;
+  const { data = {}, isEditMode, listingItems, totalPages, onPaginationChange, variation, currentPage, prevBatch, nextBatch, isFolderContentsListing, hasLoaded } = props;
 
   let ListingBodyTemplate;
   // Legacy support if template is present
@@ -31,20 +19,15 @@ const ListingBody = withQuerystringResults((props) => {
   const defaultVariation = variations.filter((item) => item.isDefault)?.[0];
 
   if (data.template && !data.variation) {
-    const legacyTemplateConfig = variations.find(
-      (item) => item.id === data.template,
-    );
+    const legacyTemplateConfig = variations.find((item) => item.id === data.template);
     ListingBodyTemplate = legacyTemplateConfig.template;
   } else {
-    ListingBodyTemplate =
-      variation?.template ?? defaultVariation?.template ?? null;
+    ListingBodyTemplate = variation?.template ?? defaultVariation?.template ?? null;
   }
 
   const listingRef = createRef();
 
-  const NoResults = variation?.noResultsComponent
-    ? variation.noResultsComponent
-    : config.blocks?.blocksConfig['listing'].noResultsComponent;
+  const NoResults = variation?.noResultsComponent ? variation.noResultsComponent : config.blocks?.blocksConfig['listing'].noResultsComponent;
 
   // const HeadlineTag = data.headlineTag || 'h2';
 
@@ -61,20 +44,14 @@ const ListingBody = withQuerystringResults((props) => {
     <>
       {listingItems?.length > 0 ? (
         <div className="listing-wrapper" ref={listingRef}>
-          <ListingBodyTemplate
-            items={listingItems}
-            isEditMode={isEditMode}
-            {...data}
-            {...variation}
-          />
+          <ListingBodyTemplate items={listingItems} isEditMode={isEditMode} {...data} {...variation} />
           {totalPages > 1 && (
             <div className="pagination-wrapper">
               <Pagination
                 activePage={currentPage}
                 totalPages={totalPages}
                 onPageChange={(e, { activePage }) => {
-                  !isEditMode &&
-                    listingRef.current.scrollIntoView({ behavior: 'smooth' });
+                  !isEditMode && listingRef.current.scrollIntoView({ behavior: 'smooth' });
                   onPaginationChange(e, { activePage });
                 }}
                 firstItem={null}
@@ -101,23 +78,14 @@ const ListingBody = withQuerystringResults((props) => {
               })}
             >
               <h2>{data.headline}</h2>
-              <p className={`content-button`}>
-                {buttonMessage['button'][lang]}
-              </p>
+              <p className={`content-button`}>{buttonMessage['button'][lang]}</p>
             </div>
           )}
         </div>
       ) : isEditMode ? (
         <div className="listing message" ref={listingRef}>
-          {isFolderContentsListing && (
-            <FormattedMessage
-              id="No items found in this container."
-              defaultMessage="No items found in this container."
-            />
-          )}
-          {hasLoaded && NoResults && (
-            <NoResults isEditMode={isEditMode} {...data} />
-          )}
+          {isFolderContentsListing && <FormattedMessage id="No items found in this container." defaultMessage="No items found in this container." />}
+          {hasLoaded && NoResults && <NoResults isEditMode={isEditMode} {...data} />}
           <Dimmer active={!hasLoaded} inverted>
             <Loader indeterminate size="small">
               <FormattedMessage id="loading" defaultMessage="Loading" />
@@ -126,9 +94,7 @@ const ListingBody = withQuerystringResults((props) => {
         </div>
       ) : (
         <div className="emptyListing">
-          {hasLoaded && NoResults && (
-            <NoResults isEditMode={isEditMode} {...data} />
-          )}
+          {hasLoaded && NoResults && <NoResults isEditMode={isEditMode} {...data} />}
           <Dimmer active={!hasLoaded} inverted>
             <Loader indeterminate size="small">
               <FormattedMessage id="loading" defaultMessage="Loading" />
